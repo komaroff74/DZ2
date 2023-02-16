8d229450ae611aa93637b4a69bd404588fa4b53d
Komarov <komaroff74@yandex.ru>
2023-02-17 00:08:49 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        int amount = 13676;
+        int bonus = amount / 20;
+        System.out.println("Ваш бонус" + bonus);
+    }
+}
